ad5ce373e8ad85c0caf11e0f0bc0e51028302acb
Brady Helkenn <bhelkenn@gmail.com>
2018-02-25 11:47:27 -0800

    no real changes

--- Tutorial Pt 2/WoB_Client_Basic_Proj_1/Assets/Scripts/TestSceneScript2.cs
+++ Tutorial Pt 2/WoB_Client_Basic_Proj_1/Assets/Scripts/TestSceneScript2.cs
@@ -8,46 +8,42 @@ public class TestSceneScript2 : MonoBehaviour {
 	public Material animal1;
 	public Material animal2;
 	public Material animal3;
+	public float timeToScaleOver;
+	public Vector3 scaleFactor; // desired factor to scale animal size to
+
 	// Use this for initialization
 	void Start () {
-				
-				//Instantiate(ball,screenPosition,Quaternion.identity);
-
-				for (var x = 0; x < 5; x++) {
-						MakeAnimal (0);
-				}
-	
+		//Instantiate(ball,screenPosition,Quaternion.identity);
+		for (var x = 0; x < 5; x++) {
+			MakeAnimal (0);
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetKeyDown ("a")) {
+			MakeAnimal (1);
+		}
+		if(Input.GetKeyDown ("s")){
+			MakeAnimal (2);
+		}
+		if(Input.GetKeyDown ("d")){
+			MakeAnimal (3);
+		}
 
-				if (Input.GetKeyDown ("a")) {
-						MakeAnimal (1);
-				}
-
-				if(Input.GetKeyDown ("s")){
-						MakeAnimal (2);
-				}
-
-				if(Input.GetKeyDown ("d")){
-						MakeAnimal (3);
-				}
-						
-	
 	}
 
-		void MakeAnimal(int type){
-				screenPosition = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0,Screen.width), Random.Range(-300,Screen.height), 40));
-				screenPosition.y = Terrain.activeTerrain.SampleHeight(screenPosition);
-				newAnimal = Instantiate(dummy, screenPosition, Quaternion.identity) as GameObject;
-				//Debug.Log (newAnimal.transform.Find("Plane").GetComponent<Renderer>().material);
-				if (type == 1) {
-						newAnimal.transform.Find ("Plane").GetComponent<Renderer>().material = animal1;
-				} else if (type == 2) {
-						newAnimal.transform.Find ("Plane").GetComponent<Renderer> ().material = animal2;
-				} else if (type == 3) {
-						newAnimal.transform.Find ("Plane").GetComponent<Renderer> ().material = animal3;
-				}
+	void MakeAnimal(int type) {
+		screenPosition = Camera.main.ScreenToWorldPoint(new Vector3(Random.Range(0,Screen.width), Random.Range(-300,Screen.height), 40));
+		screenPosition.y = Terrain.activeTerrain.SampleHeight(screenPosition);
+		newAnimal = Instantiate(dummy, screenPosition, Quaternion.identity) as GameObject;
+
+		if (type == 1) {
+			newAnimal.transform.Find ("Plane").GetComponent<Renderer>().material = animal1;
+		} else if (type == 2) {
+			newAnimal.transform.Find ("Plane").GetComponent<Renderer> ().material = animal2;
+		} else if (type == 3) {
+			newAnimal.transform.Find ("Plane").GetComponent<Renderer> ().material = animal3;
 		}
+	}
 }
